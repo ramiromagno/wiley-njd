1bf45e4b32fe8803014142a5ca551f4dca1d9086
Ramiro Magno <ramiro.magno@gmail.com>
2023-07-18 23:37:55 +0100

    all citations lead to \cite{}

--- _extensions/wiley-njd/wiley-njd.lua
+++ _extensions/wiley-njd/wiley-njd.lua
@@ -38,3 +38,15 @@ Header = function(el)
     return nil
   end
 end
+
+return {
+  Cite = function(el)
+    if quarto.doc.is_format("pdf") then
+      local cites = el.citations:map(function(cite)
+        return cite.id
+      end)
+      local citesStr = "\\cite{" .. table.concat(cites, ", ") .. "}"
+      return pandoc.RawInline("latex", citesStr)
+    end
+  end
+}
